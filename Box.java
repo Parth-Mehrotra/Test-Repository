7a78a3630da5fe1fa5b46b5da33aa3cef75f7607
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2016-01-10 21:51:58 -0500

    added class skelaton

---
+++ Box.java
@@ -0,0 +1,25 @@
+public class Box {
+
+	private int x;
+	private int y;
+	private int w;
+	private int h;
+
+	public Box() {
+		x = 0;
+		y = 0;
+		w = 0;
+		h = 0;
+	}
+
+	public Box(int x, int y, int w, int h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+
+	public int getArea() {
+		return w*h;
+	}
+}
